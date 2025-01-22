a693e70f34d74bd0d371a88f400b2daddbdebcaa
SvetDrag <svetlozardragnev@gmail.com>
2025-01-22 20:16:46 +0200

    Ex2

--- src/ArraysLab/PrintNumbersInReverseOrder02.java
+++ src/ArraysLab/PrintNumbersInReverseOrder02.java
@@ -1,10 +1,24 @@
 package ArraysLab;
 
+import java.util.Scanner;
+
 public class PrintNumbersInReverseOrder02 {
     public static void main(String[] args) {
 
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int [] number = new int[n];
+
+        for (int i = 0; i  < n; i++) {
+            int input = Integer.parseInt(scanner.nextLine());
+            number[i] = input;
+        }
+
+        for (int i = n-1; i >= 0 ; i--) {
+            System.out.print(number[i] + " ");
+        }
+
     }
 }
