2694fd4f5604c60efb3eae4409cd3cea86c6af06
SvetDrag <svetlozardragnev@gmail.com>
2025-01-22 17:19:34 +0200

    1

--- src/ArraysLab/PrintNumbersInReverseOrder02.java
+++ src/ArraysLab/PrintNumbersInReverseOrder02.java
@@ -4,5 +4,7 @@ public class PrintNumbersInReverseOrder02 {
     public static void main(String[] args) {
 
         System.out.println("Hello!");
+        System.out.println("Hello!");
+        
     }
 }
