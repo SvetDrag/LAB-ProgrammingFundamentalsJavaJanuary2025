61e2b827f7f7cdc049ad7e7ec99a2df395982410
SvetDrag <svetlozardragnev@gmail.com>
2025-02-20 18:48:55 +0200

    Students02

--- src/ObjectsAndClassesLab/Students/Main.java
+++ src/ObjectsAndClassesLab/Students/Main.java
@@ -18,13 +18,23 @@ public class Main {
             String firstName = command.split(" ")[0];
             String lastName = command.split(" ")[1];
             int age = Integer.parseInt(command.split(" ")[2]);
+            String homeTown = command.split(" ")[3];
 
-            Student student = new Student(firstName, lastName, age);
+            Student student = new Student(firstName, lastName, age, homeTown);
             studentsList.add(student);
 
 
 
             command = scanner.nextLine();
+        }
+
+        String city = scanner.nextLine();
+
+        for (Student student : studentsList){
+            if (student.getHomeTown().equals(city)){
+                System.out.println(student.getFirstName() + " " + student.getLastName() + " is " + student.getAge() + " years old " );
+            }
+
         }
 
 
--- src/ObjectsAndClassesLab/Students/Student.java
+++ src/ObjectsAndClassesLab/Students/Student.java
@@ -5,11 +5,13 @@ public class Student {
     private String firstName;
     private String lastName;
     private int age;
+    private String homeTown;
 
-    public Student(String firstName, String lastName, int age) {
+    public Student(String firstName, String lastName, int age, String homeTown) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.homeTown = homeTown;
     }
 
     public String getFirstName() {
@@ -34,5 +36,13 @@ public class Student {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getHomeTown() {
+        return homeTown;
+    }
+
+    public void setHomeTown(String homeTown) {
+        this.homeTown = homeTown;
     }
 }
