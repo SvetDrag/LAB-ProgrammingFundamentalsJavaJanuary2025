d92dc923c930297f3da48c157d76137eca022baa
SvetDrag <svetlozardragnev@gmail.com>
2025-02-26 20:32:35 +0200

    Ex1

--- src/AssociativeArrays/CountRealNumbers01.java
+++ src/AssociativeArrays/CountRealNumbers01.java
@@ -1,12 +1,44 @@
 package AssociativeArrays;
 
+
+import java.util.*;
+
 public class CountRealNumbers01 {
     public static void main(String[] args) {
 
+            Scanner scanner = new Scanner (System.in);
+
+            int[] input = Arrays.stream(scanner.nextLine().split(" ")).
+                            mapToInt(Integer::parseInt).
+                            toArray();
+
+
+            Map<Integer, Integer> numCount = new TreeMap<>();
+            //int currentCount = 1;
+
+            for (int number : input) {
+
+                if (!numCount.containsKey(number)){
+                    numCount.put(number, 1);
+                }
+                else {
+                  int  currentCount = numCount.get(number);
+                  numCount.put(number, currentCount + 1);
+
+                }
+
+            }
+
+            for (Map.Entry<Integer, Integer> entry : numCount.entrySet()){
+                System.out.println(entry.getKey() + " -> " + entry.getValue());
+
+            }
 
 
 
 
-        
+
+
+
     }
 }
--- src/AssociativeArrays/WordSynonyms02.java
+++ src/AssociativeArrays/WordSynonyms02.java
@@ -1,7 +1,31 @@
 package AssociativeArrays;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class WordSynonyms02 {
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner (System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Map<String, List> synonymsMap = new HashMap<>();
+
+
+        for (int i = 0; i < 3 ; i++) {
+
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            if (!synonymsMap.containsKey(word)){
+                
+            }
+
+        }
 
 
 
