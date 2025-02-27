18ae4d0d9206a8b3f0c5ea3fa4568cd86e85e48e
SvetDrag <svetlozardragnev@gmail.com>
2025-02-27 18:01:01 +0200

    Ex4

--- src/AssociativeArrays/WordFilter04.java
+++ src/AssociativeArrays/WordFilter04.java
@@ -1,9 +1,22 @@
 package AssociativeArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class WordFilter04 {
     public static void main(String[] args) {
 
+         Scanner scanner = new Scanner (System.in);
 
+         String [] words = Arrays.stream(scanner.nextLine()
+                          .split(" ")).filter(word -> word.length() % 2 ==0)
+                          .toArray(String [] :: new); //if lettes of word is even -> print
+
+        for (String t : words){
+            System.out.printf("%s ", t);
+        }
+
+        //Arrays.stream(words).forEach(System.out::print);
 
 
 
