cd3f9e993a1d1e06febc250809b7f5da99134dfd
SvetDrag <svetlozardragnev@gmail.com>
2025-03-13 18:06:23 +0200

    Ex1

--- src/RegularExpressions/Demo.java
+++ src/RegularExpressions/Demo.java
@@ -1,9 +1,27 @@
 package RegularExpressions;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Demo {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner (System.in);
+        String input = scanner.nextLine();
+
+        String regex = "\\b([A-Z]{1}[a-z]+)[ ]{1}([A-Z]{1}[a-z]+)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+
+
+            System.out.print(matcher.group() + " ");
+
+        }
+
 
 
     }
